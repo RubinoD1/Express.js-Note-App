c58fce098a963d66be393d34bc35b255688afd24
RubinoD1 <Rubinod1@gmail.com>
2022-07-15 19:13:03 -0400

    res.json from notes working, fixed error in notes.json file

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 //route front-end can requeest data from
-const { notes } = require('./data/notes.json'); 
+const { notes } = require('./data/notes.json');
 
 const app = express();
 
@@ -11,7 +11,10 @@ app.listen(3001, () => {
 
 
 //route to notes.json 
+/*the .get method requires two arguments
+1) a string that describes the route the client will have to fetch from.
+2) a callback function that will execute every time that route is accessed with the GET request. */
 app.get('/api/notes', (req, res) => {
-  res.send('Hello!');
+  res.json(notes);
 });
 
