20027d41df5450c9a40a0e1d159b93d6ae4e9130
RubinoD1 <Rubinod1@gmail.com>
2022-07-15 21:03:59 -0400

    app.use setup and app.post working

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const { notes } = require('./data/notes.json');
 
 const app = express();
 
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+
 app.listen(3001, () => {
     console.log(`API server now on port 3001!`);
   });
@@ -15,6 +20,7 @@ app.listen(3001, () => {
 1) a string that describes the route the client will have to fetch from.
 2) a callback function that will execute every time that route is accessed with the GET request. */
 app.get('/api/notes', (req, res) => {
+  //tells client to interpret the data response as JSON data
   res.json(notes);
 });
 
