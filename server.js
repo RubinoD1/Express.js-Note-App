231af6eaed74c9928a7d0c21a3f2a25cf9af3676
RubinoD1 <Rubinod1@gmail.com>
2022-07-17 19:09:23 -0400

    created 'routes' folder and started refactoring code into new api route files

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
+const indexRoutes = require('./routes/apiRoutes/indexRoutes');
 
-//route front-end can request data from
-const { notes } = require('./data/notes.json');
-
+//process.env.PORT is need by Heroku 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
@@ -17,6 +16,11 @@ app.get('/', (req, res) => {
 
 app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+//The * acts as a wildcard, any route that hasn't been defined will fall under this request and will go to the homepage. 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 /* app.use()
@@ -40,23 +44,9 @@ app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
 });
 
-//route to notes.json 
-/*the .get method requires two arguments
-1) a string that describes the route the client will have to fetch from.
-2) a callback function that will execute every time that route is accessed with the GET request. */
-app.get('/api/notes', (req, res) => {
-  //tells client to interpret the data response as JSON data
-  res.json(notes);
-});
 
-//route that accepts data to be used or stored server-side
-// post requests represent the action of the client requesting the server to accept data. 
-app.post('/api/notes', (req, res) => {
-  // req.body is where our incoming content will be 
-  console.log(req.body);
-  //The req.body property is where we can access the post request data and do something with it. 
-  res.json(req.body);
-});
+
+
 
 
 /*  No array function made yet*** - CHECK animalsArray in module
